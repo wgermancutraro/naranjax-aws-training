6ae1676d8720fd01f53415147755e7b61fb86929
Germán Cutraro <cutraroger@gmail.com>
2021-07-14 19:38:31 -0300

    refactor: quit gsi

--- src/clients/service/get-clients.service.js
+++ src/clients/service/get-clients.service.js
@@ -2,12 +2,10 @@ const dynamo = require('ebased/service/storage/dynamo');
 
 exports.getAllClients = () => dynamo.scanTable({ TableName: process.env.CLIENTS_TABLE_NAME });
 
-exports.getClientById = id =>
-  dynamo.queryTable({
-    TableName: process.env.CLIENTS_TABLE_NAME,
-    IndexName: 'id-index',
-    KeyConditionExpression: 'id = :id',
-    ExpressionAttributeValues: {
-      ':id': id
-    }
-  });
+exports.getClientById = async id => {
+  const clients = await dynamo.scanTable({ TableName: process.env.CLIENTS_TABLE_NAME });
+
+  const client = clients.Items?.find(client => client.id === id);
+
+  return client;
+};
